9b6dc02761a55c9e185209ba7a31917c8ce38386
Tobias Goldkette Prang <tobi8268@zbc.dk>
2021-01-27 12:46:18 +0100

    Updates

--- Airport/AirportModel.Designer.cs
+++ Airport/AirportModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\TGPGamez\source\repos\H2-Airport\ConsoleApp1\AirportModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\TGPGamez\source\repos\H2-Airport\Airport\AirportModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
